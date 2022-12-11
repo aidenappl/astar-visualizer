be2bb17af6d4d5847e64c2a1099a21092b2a71f1
Aiden Appleby <aiden.appleby3@gmail.com>
2022-12-11 17:21:51 +0000

    added setting the start and end point

--- pages/interactive.tsx
+++ pages/interactive.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import Layout from "../components/Layout";
+import { Node } from "../types/node.type";
 
 const InteractivePage = () => {
+	// Node State
 	const [nodes, setNodes] = useState<any[]>([]);
+
+	// Mouse Event Handler
+	const [clickState, setClickState] = useState<string | null>(null);
+
+	// Storing Start & Endpoint
+	const [startPoint, setStartPoint] = useState<Node | null>(null);
+	const [endPoint, setEndPoint] = useState<Node | null>(null);
 
 	const handleMouseMove = (event: any) => {
 		let node = event.target.id;
@@ -17,6 +26,28 @@ const InteractivePage = () => {
 			}
 		}
 	};
+
+	const setMasterPoint = (node: Node, pointType: string) => {
+		if (pointType == "startpoint" && startPoint == null) {
+			setStartPoint(node);
+			setNodes((prevState) => {
+				let newState = [...prevState];
+				newState[node.id].type = "startpoint";
+				return newState;
+			});
+		} else if (pointType == "endpoint" && endPoint == null) {
+			setEndPoint(node);
+			setNodes((prevState) => {
+				let newState = [...prevState];
+				newState[node.id].type = "endpoint";
+				return newState;
+			});
+		}
+	};
+
+	useEffect(() => {
+		console.log(clickState);
+	}, [clickState]);
 
 	useEffect(() => {
 		setNodes(generateNodes(700, 700));
@@ -62,11 +93,14 @@ const InteractivePage = () => {
 	return (
 		<Layout className="">
 			<div className="relative z-10 flex flex-col items-center py-16">
-				<h1 className="mb-4 text-2xl">Draw Obstacles</h1>
+				<h1 className="mb-5 text-2xl">Draw Obstacles</h1>
 				<div className="flex gap-4">
 					<Button
-						className="bg-slate-700"
+						className="!bg-slate-700"
 						onClick={() => {
+							setClickState(null);
+							setStartPoint(null);
+							setEndPoint(null);
 							setNodes(generateNodes(700, 700));
 						}}
 						value="Clear"
@@ -80,24 +114,59 @@ const InteractivePage = () => {
 				</div>
 			</div>
 			<div className="absolute top-0 flex items-center justify-center w-full h-screen z-1">
-				<div id="interactivezone" className="flex flex-wrap w-[700px]">
-					{nodes.map((node, i) => {
-						return (
-							<div
-								key={i}
-								onClick={() => {
-									selectedNode(node.id);
-								}}
-								id={`node_${node.id}`}
-								className={
-									"w-[28px] h-[28px] bg-gray-200 border border-gray-300 hover:bg-gray-800 " +
-									(node.type === "selected"
-										? "bg-gray-500"
-										: "")
-								}
-							></div>
-						);
-					})}
+				<div className="flex flex-col gap-4">
+					<div className="flex gap-2">
+						<div
+							onClick={() => {
+								setClickState("startpoint");
+							}}
+							className="w-[28px] h-[28px] bg-green-400 border border-green-700 hover:bg-green-500"
+						/>
+						<div
+							onClick={() => {
+								setClickState("endpoint");
+							}}
+							className="w-[28px] h-[28px] bg-red-400 border border-red-700 hover:bg-red-500"
+						/>
+						<div
+							onClick={() => {
+								setClickState(null);
+							}}
+							className="w-[28px] h-[28px] bg-gray-300 border border-gray-400 hover:bg-gray-800"
+						/>
+					</div>
+					<div
+						id="interactivezone"
+						className="flex flex-wrap w-[700px] "
+					>
+						{nodes.map((node, i) => {
+							return (
+								<div
+									key={i}
+									onClick={() => {
+										if (clickState == "startpoint") {
+											setMasterPoint(node, "startpoint");
+										} else if (clickState == "endpoint") {
+											setMasterPoint(node, "endpoint");
+										} else {
+											selectedNode(node.id);
+										}
+									}}
+									id={`node_${node.id}`}
+									className={
+										"w-[28px] h-[28px] bg-gray-200 border border-gray-300 hover:bg-gray-800 " +
+										(node.type === "selected"
+											? "bg-gray-500" // Selected
+											: node.type === "startpoint"
+											? "bg-green-500" // Start
+											: node.type === "endpoint"
+											? "bg-red-500" // End
+											: "")
+									}
+								/>
+							);
+						})}
+					</div>
 				</div>
 			</div>
 		</Layout>
